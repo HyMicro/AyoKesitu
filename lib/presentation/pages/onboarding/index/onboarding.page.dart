43129ce6268f4775d843ac2995ddaa8593c5f616
HyMicro <izzaahyad03@gmail.com>
2024-09-29 22:44:49 +0700

    add: onboarding init

--- lib/presentation/pages/onboarding/index/onboarding.page.dart
+++ lib/presentation/pages/onboarding/index/onboarding.page.dart
@@ -14,17 +14,53 @@ class OnboardingPage extends StatelessWidget {
 
     return GetBuilder<InitController>(builder: (controller) {
       return Scaffold(
-        body: ListView(
-          shrinkWrap: true,
-          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-          padding: const EdgeInsets.symmetric(horizontal: 17),
-          children: [
-            Text(
-              'Onboarding Page',
-              textAlign: TextAlign.center,
-              style: textTheme.titleLarge,
-            ),
-          ],
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            padding: const EdgeInsets.symmetric(horizontal: 17),
+            children: [
+              // Text(
+              //   'Onboarding Page',
+              //   textAlign: TextAlign.center,
+              //   style: textTheme.titleLarge,
+              // ),
+              const Spacer(),
+              const Text(
+                "BROMO",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "The procurement of Jeeps for the purpose of crossing Mount Bromo "
+                "is very important, because the rugged landscape requires durable "
+                "vehicles and skilled operators. Furthermore, regulations set by "
+                "the Bromo Tengger Semeru National Park further support the use of "
+                "Jeeps to facilitate safer and more effective exploration.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your navigation or action here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                child: const Text(
+                  "CONTINUE",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+              SizedBox(height: 40),
+            ],
+          ),
         ),
       );
     });
